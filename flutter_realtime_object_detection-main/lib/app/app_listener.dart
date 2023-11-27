def56726573a6f157400a3d75ac153dbb9ee6ddf
yaswanth67 <66238314+yaswanth67@users.noreply.github.com>
2023-11-27 19:54:55 +0530

    commit

---
+++ flutter_realtime_object_detection-main/lib/app/app_listener.dart
@@ -0,0 +1,31 @@
+
+import 'dart:async';
+
+class AppListener {
+  static StreamController<Map> _appManageListener = StreamController.broadcast();
+
+  static Map<Function, StreamSubscription> listeners = {};
+
+  static void addListener(void onFunc(dynamic data)) {
+    listeners[onFunc] = _appManageListener.stream.listen(onFunc);
+  }
+
+  static void removeListener(void onFunc(dynamic data)) {
+    StreamSubscription? listener = listeners[onFunc];
+    if(listener == null) return;
+    listener.cancel();
+    listeners.remove(onFunc);
+  }
+
+  static void close() {
+    _appManageListener.close();
+  }
+
+
+
+  ///
+  static void test(dynamic data){
+    _appManageListener.add(data);
+  }
+
+}
